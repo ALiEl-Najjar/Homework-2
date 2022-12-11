9e96135edecb0b8a345d526e205b45da914ee7b0
Ali El-Najjar <Alielnajjar2001@gmail.com>
2022-12-11 19:36:36 +0200

    completed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+const body = document.body;
+let lastScroll = 0;
+
+window.addEventListener('scroll', () => {
+   const currentScroll = window.pageYOffset
+
+   if(currentScroll <= 0){
+    body.classList.remove("scroll-up")
+   }
+   if(currentScroll > lastScroll && !body.classList.contains("scroll-down")){
+    body.classList.remove("scroll-up")
+    body.classList.add("scroll-down")
+   }
+   if(currentScroll < lastScroll && body.classList.contains("scroll-down")){
+    body.classList.remove("scroll-down")
+    body.classList.add("scroll-up")
+   }
+   lastScroll = currentScroll;
+})
+
 $(document).ready(function(){
     $('.filter-item').click(function(){
         const value = $(this).attr('data-filter')
@@ -14,3 +34,19 @@ $(document).ready(function(){
         $(this).addClass("active-filter").siblings().removeClass("active-filter");
     })
 })
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
